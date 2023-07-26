231ee624f70444070ae614e1041e9fb74544b4f5
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-07-26 12:16:42 -0400

    APIRouter import cleanup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from translate import router as translate_router
-from facts import router as facts_router
+from translate.translate import router as translate_router
+from facts.facts import router as facts_router
 
 # Create an instance of the FastAPI app
 app = FastAPI()
@@ -14,21 +14,6 @@ app.include_router(facts_router, prefix="/fact_for_number")
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-# Define a route to post translations
-@app.post("/translate/")
-def translate(text: str):
-    """Get a German response"""
-
-    return {"translation": "Guten Tag."}
-
-
-@app.post("/fact_for_number/")
-def random_fact(word: str):
-    """Get a random fact for a word"""
-
-    return {"fact": "42 is the reponse to the most important question"}
 
 
 # Run the application using uvicorn server
