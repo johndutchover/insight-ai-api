5be8d004fcfc359cc6a672410020c32c35fcab20
Robin Beer <r.beer@outlook.de>
2023-07-29 16:12:51 +0000

    type annotation for random_fact function

--- app/routers/facts.py
+++ app/routers/facts.py
@@ -4,6 +4,6 @@ router = APIRouter()
 
 
 @router.post("/fact_for_number")
-def random_fact():
+def random_fact() -> dict[str,str]:
     """Get a random fact for a number"""
     return {"fact": "10 is the base of the decimal numeral system."}
