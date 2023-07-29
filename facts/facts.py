71c38659160fa07b419ca93cce9c77fee56198d5
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-07-28 20:02:16 -0400

    refactor routes

--- facts/facts.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.post("/")
-def random_fact(word: str):
-    """Get a random fact for a word"""
-    return {"fact": "42 is the response to the most important question"}
--- translate/translate.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.post("/")
-def translate(text: str):
-    """Get a German response"""
-    return {"translation": "Guten Tag."}
