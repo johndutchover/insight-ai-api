51f217a1ce3836e9ff40691166a8dd9d9d65ee74
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-01 20:17:13 -0400

    placeholder endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+from pydantic import BaseModel, Field
 from fastapi import FastAPI
 from app.routers import facts, translate
 
@@ -5,6 +7,18 @@ app = FastAPI()
 
 app.include_router(facts.router, prefix="/facts", tags=["facts"])
 app.include_router(translate.router, prefix="/translate", tags=["translate"])
+
+marvin_openai_api_key = os.environ.get("MARVIN_OPENAI_API_KEY")
+
+
+class Location(BaseModel):
+    city: str
+    state: str = Field(..., description="The two-letter state abbreviation")
+
+
+@app.post("/location/")
+def create_location(location: Location):
+    return {"location": location}
 
 
 @app.get("/")
