5ac3671a7c0f64b4ff6e0ea3da439fa36248557e
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-02 13:25:00 -0400

    update type annotation for random_fact function

--- app/routers/facts.py
+++ app/routers/facts.py
@@ -1,9 +1,14 @@
+import marvin
 from fastapi import APIRouter
+from marvin import ai_fn
 
 router = APIRouter()
 
+marvin.settings.llm_model = "openai/gpt-3.5-turbo"
+
 
 @router.post("/fact_for_number")
-def random_fact() -> dict[str, str]:
-    """Get a random fact for a number"""
-    return {"fact": "10 is the base of the decimal numeral system."}
+@ai_fn
+def random_fact(n: int) -> dict[int, str]:
+    """Get a random fact for `n` and return both as a dictionary with `n` as key
+    and the random fact as value"""
