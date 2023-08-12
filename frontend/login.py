d48fca997a38eb8a37a3068b38acff3d460c76e1
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-11 20:04:47 -0400

    add frontend as separate project

---
+++ frontend/login.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pathlib
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit_extras.switch_page_button import switch_page
+
+# Path to the virtual environment
+venv_path = pathlib.Path("venv-frontend/bin/activate")
+
+# Modify the PATH environment variable to include the venv
+os.environ["PATH"] = str(venv_path) + os.pathsep + os.environ["PATH"]
+
+# Modify the system path to include the venv
+sys.path.insert(0, str(venv_path))
+
+env_path = pathlib.Path(__file__).parent / ".env"
+load_dotenv(env_path)
+
+
+# Check password function
+def check_password():
+    # Check if password has been entered correctly
+    if st.session_state.get("password_correct"):
+        return True
+    else:
+        password = st.text_input("Password", type="password")
+        if password == os.environ.get("PASSWORD"):
+            st.session_state["password_correct"] = True
+            return True
+        else:
+            st.error("Incorrect password")
+            return False
+
+
+# Prompt for password
+if check_password():
+    # Set a flag in the session state to indicate that the password is correct
+    st.session_state["password_correct"] = True
+    # Redirect using extras switch_page
+    switch_page("app")
