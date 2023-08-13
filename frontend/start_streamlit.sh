00e31628eaa957877aae8e7c47bd843f192c07d4
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-13 16:38:33 -0400

    adjust start_streamlit.sh to work local or in fly

--- frontend/start_streamlit.sh
+++ frontend/start_streamlit.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-# Get the local IP address
-LOCAL_IP_ADDRESS=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1)
-
-# Export it as an environment variable
-export LOCAL_IP_ADDRESS
-
-# Print the value
-echo "Local IP address set as: $LOCAL_IP_ADDRESS"
-
-# streamlit run pages/1_Home.py --server.port=8501 --server.address=$LOCAL_IP_ADDRESS
-streamlit run login.py --server.port=8501 --server.address="$LOCAL_IP_ADDRESS"
+#!/bin/bash
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS command to get local IP address
+  LOCAL_IP_ADDRESS=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1)
+  # Run Streamlit with the LOCAL_IP_ADDRESS
+  streamlit run login.py --server.port=8501 --server.address="$LOCAL_IP_ADDRESS"
+else
+  # Assuming a Linux environment, just run Streamlit without specifying the address
+  streamlit run login.py --server.port=8501
+fi
