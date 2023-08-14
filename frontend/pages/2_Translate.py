b77c87ff4bf92b5efa40a0243901856fbfbe325e
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-13 20:37:43 -0400

    German translate only

--- frontend/pages/2_Translate.py
+++ frontend/pages/2_Translate.py
@@ -1,35 +1,27 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
 import requests
 
+BASE_URL = "https://insight-ai-api.fly.dev/"
 
-def translate_german(text):
-    api_url = f"https://insight-ai-api.fly.dev/translate/translate?text={text}"
-    headers = {"accept": "application/json"}
-    response = requests.post(api_url, headers=headers, data="", timeout=5)
+
+def translate_eng_de(text):
+    endpoint = f"translate/translate?text={text}"
+    url = BASE_URL + endpoint
+    response = requests.post(url, timeout=5)
 
     if response.status_code == 200:
-        return response.json().get("translation", "")
+        return response.text
     else:
         st.error("Failed to translate the text.")
         return ""
 
 
-def translate_english(text):
-    return translate_german(text)
-
-
 st.title("Translators")
 
-# Check for the translation button click
-if st.button("German to English"):
-    user_input = st.text_input("Enter the German word you want to translate:")
-    if user_input:
-        translation = translate_english(user_input)
-        st.write(f"Translation: {translation}")
-    else:
-        st.write("Please enter a word to translate.")
+user_input = st.text_area("Enter the English word you want to translate:")
 
-# Check for the home button click
-if st.button("Home"):
-    switch_page("Home")
+if st.button("Translate to German"):
+    if user_input:
+        translation = translate_eng_de(user_input)
+        if translation:
+            st.write(f"Translation: {translation}")
