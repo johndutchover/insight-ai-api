91fd0ff1502f6463d14ca67c1777f4a249e75415
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-14 11:02:49 -0400

    fixes for fly deploy frontend

--- frontend/start_streamlit.sh
+++ frontend/start_streamlit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS command to get local IP address
   LOCAL_IP_ADDRESS=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1)
