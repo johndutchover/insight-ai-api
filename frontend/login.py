eac12e2909f53c9489f5e11067bde43b5b90205f
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2023-08-14 11:20:25 -0400

    fixes for fly deploy frontend FE_PWD

--- frontend/login.py
+++ frontend/login.py
@@ -6,19 +6,32 @@ from streamlit_extras.switch_page_button import switch_page
 load_dotenv(".env")
 
 
-# Check password function
 def check_password():
-    # Check if password has been entered correctly
+    """Check if the password has been entered correctly."""
     if st.session_state.get("password_correct"):
         return True
+
+    password = st.text_input("Password", type="password")
+    expected_password = os.environ.get("FE_PWD")
+
+    if expected_password is None:
+        st.error("Password not configured")
+        return False
+
+    if password == expected_password:
+        st.session_state["password_correct"] = True
+        return True
     else:
-        password = st.text_input("Password", type="password")
-        if password == os.environ.get("PASSWORD"):
-            st.session_state["password_correct"] = True
-            return True
-        else:
-            st.error("Incorrect password")
-            return False
+        st.error("Incorrect password")
+        return False
+
+
+# Prompt for password
+if check_password():
+    # Set a flag in the session state to indicate that the password is correct
+    st.session_state["password_correct"] = True
+    # Redirect using extras switch_page
+    switch_page("Home")
 
 
 # Prompt for password
