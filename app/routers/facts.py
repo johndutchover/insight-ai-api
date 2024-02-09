d70b0e584a902846946842925d90836d22a2ec45
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2024-02-09 16:32:59 -0500

    .pre-commit-config.yaml

--- app/routers/facts.py
+++ app/routers/facts.py
@@ -1,19 +1,11 @@
-import os
 import marvin
 
 from fastapi import APIRouter
-from marvin import ai_fn
 
 router = APIRouter()
 
-marvin.settings.llm_model = "openai/gpt-3.5-turbo"
-marvin_openai_api_key = os.environ.get("MARVIN_OPENAI_API_KEY")
 
-# Set the OpenAI API key in marvin's settings
-marvin.settings.openai.api_key = marvin_openai_api_key
-
-
-@ai_fn
+@marvin.fn
 def random_fact(n: int) -> dict[int, str]:
     """
     Get a random fact for `n` and return it in a dictionary with `n` as the key.
