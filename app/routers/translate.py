4bcb9923400a5805d0d8eb215f8f2cbdd46a40bb
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2024-03-27 12:39:10 -0400

    translate.py function

--- app/routers/translate.py
+++ app/routers/translate.py
@@ -6,11 +6,15 @@ router = APIRouter()
 
 @marvin.fn
 def translate_ai(text: str) -> str:
-    """Given the input `text`, return a translation to czech."""
+    """Given the input `text`, return a translation to input language `language`."""
+
+
+@marvin.fn
+def translate_service(text: str) -> str:
+    """Target language service translates my_language to target_language."""
 
 
 @router.post("/translate")
-def translate(text: str) -> str:
-    """Translate input `text` to czech."""
-    # """Translate input `text` to the given output language `language`."""
-    return translate_ai(text)
+def translate(text: str, my_language: str, target_language: str) -> str:
+    """Return the translation of `text` from `my_language` to language `target_language`."""
+    return translate_service(text)
